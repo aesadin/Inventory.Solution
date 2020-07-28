dbdd40e6814b465fee49ba0736fdd3c815371e55
Allison Sadin <aesadin@gmail.com>
2020-07-28 16:02:15 -0700

    created inventories controller

--- Inventory/Controllers/HomeController.cs
+++ Inventory/Controllers/HomeController.cs
@@ -1,1 +1,15 @@
-//Home Controller will handle the central homepage
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+  }
+}
--- Inventory/Controllers/InventoriesController.cs
+++ Inventory/Controllers/InventoriesController.cs
@@ -2,3 +2,34 @@ using Microsoft.AspNetCore.Mvc;
 using Inventory.Models;
 using System.Collections.Generic;
 using System.Linq;
+
+namespace Inventory.Controllers
+{
+  public class InventoriesController : Controller
+  {
+    private readonly InventoryContext _db;
+
+    public InventoriesController(InventoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Inventory> model = _db.Inventory.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    public ActionResult Create(Inventory inventory)
+    {
+      _db.Inventories.Add(inventory);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
